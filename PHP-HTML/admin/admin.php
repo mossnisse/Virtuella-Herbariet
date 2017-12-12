1527d180ff0af3ffceaac1402399b9705841d1e5
Nils Ericson <mossnisse@hotmail.com>
2017-12-12 10:30:42 +0100

    Add files via upload

---
+++ PHP-HTML/admin/admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    
+<head>
+    <title> Virtuella herbariet Admin page </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+    <div>
+        <a href="replace.php">updatera databasen LD, UMU...</a> <br />
+        <a href="replaceLINREG.php">updatera databasen LD med LINREG...</a> <br />
+        <a href="replaceImage.php">updatera databasen LD med Image...</a> <br />
+        <a href="replaceUPS.php">updatera databasen UPS...</a> <br />
+        <a href="replaceGB.php">updatera databasen GB...</a> <br />
+        <a href="replaceS.php">updatera databasen S Kärlväxter </a> <br />
+        <a href="replaceKryptoS.php">updatera databasen KryptoS...</a> <br />
+        <a href="replaceOHN.php">updatera databasen OHN...</a> <br />
+        <a href="insertRevOHN.php">sätt in revisions fil OHN</a> <p />
+        <a href="delete.php">ta bort fil från databasen...</p> <br />
+        <a href="empty_cache.php">töm cache för crossbrowser och listorna i standard search</a> <br />
+        <a href="id_links.php">räkna om länkar</a> <br />
+        <a href="redoCoordinates.php">räkna om koordinaterna..</a> <p />
+        <a href="stat.php">användar statistik</a> <br />
+        <a href="lasUpp.php">Lås upp databasen...</a> <p />
+        <a href="..\edit\samlare.php">tolkade samlare</a> <br />
+        <a href="..\edit\signatures.php">signaturer</a> <br />
+        <a href="..\edit\lokaler.php">lokal databasen</a> <br />
+        <a href="../">start page</a> <br />
+    </div>
+</body>
+</html>
