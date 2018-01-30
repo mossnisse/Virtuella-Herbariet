c4019ad51c26891b819f060958196a04e8e231ce
Nils Ericson <mossnisse@hotmail.com>
2018-01-30 15:11:42 +0100

    Add files via upload

---
+++ PHP-HTML/admin/lifewatch_sfile.php
@@ -0,0 +1,21 @@
+<?php
+include("../herbes.php");
+
+$con = conDatabase($MySQLHost, $MySQLDB, $MySQLSUser, $MySQLSPass);
+
+$query = "SELECT ID, name, date, inst, nr_records FROM sfiles where nr_records > 0";
+
+
+ $result = $con->query($query);
+    if (!$result) {
+        echo mysql_error();
+    }
+	$row = $result->fetch();
+	echo "{ \"sfiles\": [";
+	echo "{\"ID\": \"$row[ID]\", \"name\": \"$row[name]\", \"date\": \"$row[date]\", \"inst\": \"$row[inst]\", \"nr_records\": \"$row[nr_records]\"}";
+    while($row = $result->fetch())
+    {
+		echo ", {\"ID\": \"$row[ID]\", \"name\": \"$row[name]\", \"date\": \"$row[date]\", \"inst\": \"$row[inst]\", \"nr_records\": \"$row[nr_records]\"}";
+    }
+	echo "]}";
+?>
