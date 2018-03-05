72e45dc264ddc84bdda735d160453dda0753be78
Nils Ericson <mossnisse@hotmail.com>
2018-03-05 12:13:57 +0100

    lagt till koll för osynliga tecken för släktnamn

--- PHP-HTML/admin/rapportSläkten.php
+++ PHP-HTML/admin/rapportSläkten.php
@@ -12,6 +12,20 @@ echo "<head>
 	 </head>
 	 <body>
 	 <H3>Släkten</H3>
+	 mellanslag eller tabb i slutet eller början av släktnamnet
+	 <table>";
+	 
+$query = "Select ID, AccessionNo, Genus, Species, Original_name, Country from specimens where specimens.sFile_ID =$fileID and (not trim(Genus) = Genus or not trim('\\t' from Genus) = Genus) LIMIT 1000;";
+$result = $con->query($query);
+
+while($row = $result->fetch()) {
+	echo "<TR><TD><A href=\"record.php?ID=$row[ID]\">$row[AccessionNo]</A></TD><TD>$row[Genus]</TD><TD>$row[Species]</TD><TD>$row[Original_name]</TD><TD>$row[Country]</TD></TR>";
+}	 
+
+	 
+echo "
+	 </table>
+	 <p />
 	 Släkten som saknas i Systematiktabellen eller är felstavade vid registreringen. Visar max 1000 poster
 	 <TABLE>
 		<TR><TD>Catalogue No.</TD><TD>Genus</TD><TD><Species/TD><TD>Original name</TD><TD>Country</TD></TR>";
