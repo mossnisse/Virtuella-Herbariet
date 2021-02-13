60ea6c9a02997aee46e2c7c622de3759b81e051d
Nils Ericson <mossnisse@hotmail.com>
2021-02-13 14:42:02 +0100

    UTM support

--- PHP-HTML/coord/coords.js
+++ PHP-HTML/coord/coords.js
@@ -73,7 +73,7 @@ function checkC() {
 			c1 = c2;
 			c2 = ctemp2;
 		}
-		if (c1>-90 && c1<90 && c2>-180 && c2<180) {
+		if (c1>=-90 && c1<=90 && c2>=-180 && c2<=180) {
 			if  (!isNaN(c1) && !isNaN(c2) && c1!=="" && c2 !=="") {
 				sys = "WGS84";
 				interpreted = c1+"N, "+c2+"E";
@@ -99,7 +99,18 @@ function checkC() {
 			WGS84 = Sweref99TMtoWGS84(Sweref99TM);
 			RT90 = WGS84toRT90(WGS84);
 			RUBIN = RT90toRUBIN(RT90);
+		} else
+		console.log(coord);
+		if (nrSpaces == 2) {
+			sys = "UTM";
+			interpreted = coord;
+			UTM = coord.split(" ");
+			WGS84 = UTMtoWGS84(UTM);
+			Sweref99TM = WGS84toSweref99TM(WGS84);
+			RT90 = WGS84toRT90(WGS84);
+			RUBIN = RT90toRUBIN(RT90);
 		}
+		
 	//} else {
 		//checking and parsing RUBIN
 		var isRUBIN = false;
@@ -178,7 +189,7 @@ function checkC() {
 		document.getElementById("interpred").innerHTML = sys + `: ` + interpreted;
 		
 		if (WGS84 !== "") {
-			document.getElementById("WGS84").innerHTML = WGS84[0].toPrecision(6)+", "+WGS84[1].toPrecision(6);
+			document.getElementById("WGS84").innerHTML = WGS84[0].toPrecision(8)+", "+WGS84[1].toPrecision(8);
 			var WGSDMS = WGS84toDMS(WGS84);
 			document.getElementById("WGS84DMS").innerHTML = WGSDMS;
 			if (Sweref99TM != "outside defined area") {
@@ -196,6 +207,9 @@ function checkC() {
 			if (sys.substring(0,5) == "RUBIN") {
 				document.getElementById("showRUBIN").disabled = false;
 			}
+			var UTM = WGS84toUTM(WGS84);
+			document.getElementById("UTM").innerHTML = UTM[0]+" "+UTM[1]+", "+UTM[2];
+			document.getElementById("MGRS").innerHTML = WGS84toMGRS(WGS84);
 			getDistrict(WGS84);
 			getProvince(WGS84);
 			getCountry(WGS84);
@@ -222,253 +236,6 @@ function countC (str, ch) {
 	return result;
 }
 
-
-function RT90toWGS84(RT90) {
-	north = RT90[0];
-	east = RT90[1];
-	var xi = ((north+667.711) / 6367484.87);
-	var ny = (east - 1500064.274) / 6367484.87;
-	var s1 = 0.0008377321684;
-	var s2 = 5.905869628E-8;
-	var xp = xi - s1 * Math.sin(2*xi) * Math.cosh(2*ny) - s2 * Math.sin(4*xi) * Math.cosh(4*ny);
-	var np = ny - s1 * Math.cos(2*xi) * Math.sinh(2*ny) - s2 * Math.cos(4*xi) * Math.sinh(4*ny);
-	var reast = (0.2758717076 + Math.atan(Math.sinh(np)/Math.cos(xp)))* 180/Math.PI;
-	var qs = Math.asin(Math.sin(xp)/Math.cosh(np));
-	var rnorth = (qs + Math.sin(qs)*Math.cos(qs)*(0.00673949676 -0.00005314390556 * Math.pow(Math.sin(qs),2)) + 5.74891275E-7 * Math.pow(Math.sin(qs),4)) * 180/Math.PI;
-	//alert("RT90 to WGS84: north "+ north + " east "+east + " xi " +xi + " ny " + ny +" xp " + xp + "np" + np);
-	return [rnorth , reast];
-}
-
-
-function WGS84toRT90(WGS84) {
-	if (WGS84[0] >= 54.9 && WGS84[1] >= 10.0 && WGS84[0] <= 69.13 && WGS84[1] <= 24.2) {
-		north = WGS84[0];
-		east = WGS84[1];
-		var k0xa = 6.3674848719179137e6;
-		var FN = -667.711;			//false northing 
-		var FE = 1.500064274e6;		//false easting 
-		var A = 0.006694380021;
-		var B = 0.00003729560209;
-		var C = 2.592527517e-7;
-		var Dp = 1.971698945e-9;
-		var lambdanoll = 0.27587170754507245;  //longitude of the central meridian 
-		var beta1 = 0.0008377318249;
-		var beta2 = 7.608527793e-7;
-		var beta3 = 1.197638020e-9;
-		var beta4 = 2.443376245e-12;
-		var Phi = (north/180)*Math.PI;	//Geodetic latitude in radians	
-		var deltalambda= (east/180)*Math.PI-lambdanoll;
-		var Phistar = Phi-Math.sin(Phi)*Math.cos(Phi)*(A+B*Math.sin(2*Phi)+C*Math.sin(4*Phi)+Dp*Math.sin(6*Phi));
-		var xifjutt = Math.atan(Math.tan(Phistar)/Math.cos(deltalambda));
-		var etafjutt = Math.atanh(Math.cos(Phistar)*Math.sin(deltalambda));
-		var rnorth =  k0xa*(xifjutt+beta1*Math.sin(2*xifjutt)*Math.cosh(2*etafjutt)+beta2*Math.sin(4*xifjutt)*Math.cosh(4*etafjutt)+beta3*Math.sin(6*xifjutt)*Math.cosh(6*etafjutt)+beta4*Math.sin(8*xifjutt)*Math.cosh(8*etafjutt))+FN;
-		var reast = k0xa*(etafjutt+beta1*Math.cos(2*xifjutt)*Math.sinh(2*etafjutt)+beta2*Math.cos(4*xifjutt)*Math.sinh(4*etafjutt)+beta3*Math.cos(6*xifjutt)*Math.sinh(6*etafjutt)+beta4*Math.cos(8*xifjutt)*Math.sinh(8*etafjutt))+FE;
-		return [Math.round(rnorth), Math.round(reast)];
-	} else {
-		return "outside defined area";
-	}
-}
-
-
-function Sweref99TMtoWGS84(Sweref99TM) {
-	north = Sweref99TM[0];
-	east = Sweref99TM[1];
-	var sweref99TM_flattening = 1.0 / 298.257222101; // GRS 80.
-	var sweref99TM_axis = 6378137.0; // GRS 80.
-	var sweref99TM_centralMeridian = 0.26179938779914943653855361527329; //radians
-	var sweref99TM_false_easting = 500000.0;
-	var sweref99TM_scale = 0.9996;
-	var e2 = sweref99TM_flattening * (2.0 - sweref99TM_flattening);
-	var n = sweref99TM_flattening / (2.0 - sweref99TM_flattening);
-	var a_roof = sweref99TM_axis / (1.0 + n) * (1.0 + n * n / 4.0 + n * n * n * n / 64.0);
-	var delta1 = n / 2.0 - 2.0 * n * n / 3.0 + 37.0 * n * n * n / 96.0 - n * n * n * n / 360.0;
-	var delta2 = n * n / 48.0 + n * n * n / 15.0 - 437.0 * n * n * n * n / 1440.0;
-	var delta3 = 17.0 * n * n * n / 480.0 - 37 * n * n * n * n / 840.0;
-	var delta4 = 4397.0 * n * n * n * n / 161280.0;
-	var Astar = e2 + e2 * e2 + e2 * e2 * e2 + e2 * e2 * e2 * e2;
-	var Bstar = -(7.0 * e2 * e2 + 17.0 * e2 * e2 * e2 + 30.0 * e2 * e2 * e2 * e2) / 6.0;
-	var Cstar = (224.0 * e2 * e2 * e2 + 889.0 * e2 * e2 * e2 * e2) / 120.0;
-	var Dstar = -(4279.0 * e2 * e2 * e2 * e2) / 1260.0;
-	        // Convert.
-	var lambda_zero = sweref99TM_centralMeridian;
-	var xi = (north) / (sweref99TM_scale * a_roof);
-	var eta = (east - sweref99TM_false_easting) / (sweref99TM_scale * a_roof);
-	var xi_prim = xi -
-	                delta1 * Math.sin(2.0 * xi) * Math.cosh(2.0 * eta) -
-	                delta2 * Math.sin(4.0 * xi) * Math.cosh(4.0 * eta) -
-	                delta3 * Math.sin(6.0 * xi) * Math.cosh(6.0 * eta) -
-	                delta4 * Math.sin(8.0 * xi) * Math.cosh(8.0 * eta);
-	var eta_prim = eta -
-	                delta1 * Math.cos(2.0 * xi) * Math.sinh(2.0 * eta) -
-	                delta2 * Math.cos(4.0 * xi) * Math.sinh(4.0 * eta) -
-	                delta3 * Math.cos(6.0 * xi) * Math.sinh(6.0 * eta) -
-	                delta4 * Math.cos(8.0 * xi) * Math.sinh(8.0 * eta);
-	var phi_star = Math.asin(Math.sin(xi_prim) / Math.cosh(eta_prim));
-	var delta_lambda = Math.atan(Math.sinh(eta_prim) / Math.cos(xi_prim));
-	var lon_radian = lambda_zero + delta_lambda;
-	var lat_radian = phi_star + Math.sin(phi_star) * Math.cos(phi_star) *
-	                (Astar +
-	                Bstar * Math.pow(Math.sin(phi_star), 2) +
-	                Cstar * Math.pow(Math.sin(phi_star), 4) +
-	                Dstar * Math.pow(Math.sin(phi_star), 6));
-	var wnorth = lat_radian*180/Math.PI;
-	var weast = lon_radian*180/Math.PI;
-	return [wnorth, weast];
-}
-
-
-function WGS84toSweref99TM(WGS84) {
-	if (WGS84[0] >= 54.9 && WGS84[1] >= 10.0 && WGS84[0] <= 69.13 && WGS84[1] <= 24.2) {
-	north = WGS84[0];
-	east = WGS84[1];
-	var sweref99TM_flattening = 1.0 / 298.257222101; // GRS 80.
-	var sweref99TM_axis = 6378137.0; // GRS 80.
-	var sweref99TM_centralMeridian = 0.26179938779914943653855361527329; //radians
-	var sweref99TM_false_easting = 500000.0;
-	var sweref99TM_scale = 0.9996;
-	
-	var e2 = sweref99TM_flattening * (2.0 - sweref99TM_flattening);
-	var n = sweref99TM_flattening / (2.0 - sweref99TM_flattening);
-	var a_roof = sweref99TM_axis / (1.0 + n) * (1.0 + n * n / 4.0 + n * n * n * n / 64.0);
-	    //double A = e2;
-	var B = (5.0 * e2 * e2 - e2 * e2 * e2) / 6.0;
-	var C = (104.0 * e2 * e2 * e2 - 45.0 * e2 * e2 * e2 * e2) / 120.0;
-	var D = (1237.0 * e2 * e2 * e2 * e2) / 1260.0;
-	var beta1 = n / 2.0 - 2.0 * n * n / 3.0 + 5.0 * n * n * n / 16.0 + 41.0 * n * n * n * n / 180.0;
-	var beta2 = 13.0 * n * n / 48.0 - 3.0 * n * n * n / 5.0 + 557.0 * n * n * n * n / 1440.0;
-	var beta3 = 61.0 * n * n * n / 240.0 - 103.0 * n * n * n * n / 140.0;
-	var beta4 = 49561.0 * n * n * n * n / 161280.0;
-			
-	var phi = north * Math.PI/180;
-    var lambda = east * Math.PI/180;
-    var lambda_zero = sweref99TM_centralMeridian;
-
-    var phi_star = phi - Math.sin(phi) * Math.cos(phi) * (e2 +
-                B * Math.pow(Math.sin(phi), 2) +
-                C * Math.pow(Math.sin(phi), 4) +
-                D * Math.pow(Math.sin(phi), 6));
-    var delta_lambda = lambda - lambda_zero;
-    var xi_prim = Math.atan(Math.tan(phi_star) / Math.cos(delta_lambda));
-    var eta_prim = Math.atanh(Math.cos(phi_star) * Math.sin(delta_lambda));
-    var x = sweref99TM_scale * a_roof * (xi_prim +
-                beta1 * Math.sin(2.0 * xi_prim) * Math.cosh(2.0 * eta_prim) +
-                beta2 * Math.sin(4.0 * xi_prim) * Math.cosh(4.0 * eta_prim) +
-                beta3 * Math.sin(6.0 * xi_prim) * Math.cosh(6.0 * eta_prim) +
-                beta4 * Math.sin(8.0 * xi_prim) * Math.cosh(8.0 * eta_prim));
-    var y = sweref99TM_scale * a_roof * (eta_prim +
-                beta1 * Math.cos(2.0 * xi_prim) * Math.sinh(2.0 * eta_prim) +
-                beta2 * Math.cos(4.0 * xi_prim) * Math.sinh(4.0 * eta_prim) +
-                beta3 * Math.cos(6.0 * xi_prim) * Math.sinh(6.0 * eta_prim) +
-                beta4 * Math.cos(8.0 * xi_prim) * Math.sinh(8.0 * eta_prim)) +
-        		sweref99TM_false_easting;
-	return [Math.round(x), Math.round(y)];
-	} else {
-		return "outside defined area";
-	}
-}
-
-function RUBINtoRT90(interpreted) {
-	var n1,e1,n2,e2,n3,e3 = 0;
-	if (interpreted.length==3) {
-		// 50x50 km
-		n1 = interpreted.substring(0,2);
-		e1 = interpreted.charCodeAt(2)-65;
-		north = 6075000+n1*50000;
-		east = 1225000+e1*50000;
-		return [north,east];
-	}
-	else if (interpreted.length==5){
-		// 5x5 km
-		n1 = interpreted.substring(0,2);
-		e1 = interpreted.charCodeAt(2)-65;
-		n2 = interpreted.substring(3,4);
-		e2 = interpreted.charCodeAt(4)-97;
-		north = 6052500+n1*50000+n2*5000.0;
-		east = 1202500+e1*50000+e2*5000.0;
-		return [north,east];
-	}
-	else if (interpreted.length==10 && interpreted.substring(7,8) == "-") {  // && interpreted.substring(7,8) == "-")
-		// 1x1 km
-		n1 = interpreted.substring(0,2);
-		e1 = interpreted.charCodeAt(2)-65;
-		n2 = interpreted.substring(3,4);
-		e2 = interpreted.charCodeAt(4)-97;
-		n3 = interpreted.substring(6,7);
-		e3 = interpreted.substring(8,9);
-		north = 6050500+n1*50000+n2*5000.0+n3*1000;
-		east = 1200500+e1*50000+e2*5000.0+e3*1000;
-		return [north,east];
-	}
-	else if (interpreted.length==10) {
-		// 100 x 100 m
-		n1 = interpreted.substring(0,2);
-		e1 = interpreted.charCodeAt(2)-65;
-		n2 = interpreted.substring(3,4);
-		e2 = interpreted.charCodeAt(4)-97;
-		n3 = interpreted.substring(6,8);
-		e3 = interpreted.substring(8,10);
-		north = 6050050+n1*50000+n2*5000.0+n3*100;
-		east = 1200050+e1*50000+e2*5000.0+e3*100;
-		return [north,east];
-	}
-	else {
-		// error
-		return "error in RUBIN to RT90 conversion";
-	}
-}
-
-function RT90toRUBIN(RT90) {
-	if (RT90[0] >= 6100000 && RT90[1] >= 1200000 && RT90[0] >= 1180000 && RT90[1] <= 1890000) {
-		var n = Math.round(RT90[0])-6050000;
-		var e = Math.round(RT90[1])-1200000;
-		var n1 = Math.floor(n/50000);
-		var n1a = "";
-		if (n1<10) n1a ="0"+n1; else n1a = ""+n1;
-		var n2 = Math.floor((n%50000)/5000);
-		var n3 = Math.floor((n%5000)/100);
-		var n3a = "";
-		if (n3<10) n3a="0"+n3; else n3a = ""+n3;
-		var e1 = Math.floor(e/50000);
-		var e1a = String.fromCharCode(e1 + 65);
-		var e2 = Math.floor((e%50000)/5000);
-		var e2a = String.fromCharCode(e2 + 97);
-		var e3 = Math.floor((e%5000)/100);
-		var e3a = "";
-		if (e3 <10) e3a ="0"+e3; else e3a = ""+e3;
-		return n1a+e1a+n2+e2a+" "+n3a+e3a;
-	} else {
-		return "outside defined area";
-	}
-}
-
-function DMStoWGS84(north, east) {
-	if (latdir == "S")
-			 this.north = -latdeg-latmin/60-latsec/3600;
-		 else
-			 this.north = latdeg+latmin/60+latsec/3600;
-		 if (longdir == "W")
-			 this.east =  -longdeg-longmin/60-longsec/3600;
-		 else
-			 this.east =  longdeg+longmin/60+longsec/3600;
-}
-
-function WGS84toDMS(WGS84) {
-	var north = WGS84[0];
-	var east = WGS84 [1];
-	var NDeg = Math.floor(north);
-	var EDeg = Math.floor(east);
-	var NMin = Math.floor((north-NDeg)*60);
-	var EMin = Math.floor((east-EDeg)*60);
-	var NSec = Math.round((((north-NDeg)*60)-NMin)*60);
-	var ESec = Math.round((((east-EDeg)*60)-EMin)*60);
-	var NDir = "";
-	var EDir = "";
-	if (north<0) {NDir = "S"; NDeg = -NDeg;} else {NDir = "N";}
-	if (east<0) {EDir = "W"; EDeg = - EDeg;} else {EDir = "E";}
-	var DMS = NDeg+"&deg; "+NMin+"&prime; "+NSec+"&Prime; "+NDir+", "+EDeg+"&deg; "+EMin+"&prime; "+ESec+"&Prime; "+EDir; 
-	return DMS;
-}
-
 function ajax(url, doit)
 {
 	var xmlhttp;
@@ -488,7 +255,6 @@ function ajax(url, doit)
 	    }
 	};
 	xmlhttp.open("GET", url ,true);
-	xmlhttp.setRequestHeader("Accept-Charset","UTF-8");
 	xmlhttp.send(null);
 }
 
