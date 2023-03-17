87a2cdb4e4e8b104e48a45df7dff1fa5fc6b799b
Nils Ericson <mossnisse@hotmail.com>
2023-03-17 09:58:32 +0100

    corrected html

--- PHP-HTML/collector.php
+++ PHP-HTML/collector.php
@@ -1,5 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Sweden's Virtual Herbarium: Collector's record</title>
+    <link rel="stylesheet" href="herbes.css" type="text/css" />
+    <meta name="author" content="Nils Ericson" />
+    <meta name="robots" content="noindex" />
+    <meta name="keywords" content="Virtuella herbariet" />
+    <link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body id = "collector">
+    <div class = "menu1">
+        <ul>
+            <li class = "start_page"><a href="index.html">Start page</a></li>
+            <li class = "standard_search"><a href="standard_search.html">Search specimens</a></li>
+            <li class = "cross_browser"><a href ="cross_browser.php?SpatLevel=0&amp;SysLevel=0&amp;Sys=Life&amp;Spat=World&amp;Herb=All">Cross browser</a></li>
+            <li class = "locality_search"><a href="locality_search.php">Search localities</a></li>
+        </ul>
+    </div>
+    <div class = "subMenu">
+	<h2><span class = "first">S</span>weden's <span class = "first">V</span>irtual <span class = "first">H</span>erbarium: Collector's record</h2>
 
 <?php
 // Code Written By Nils Ericson 2010-01-04
@@ -8,17 +28,6 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 include("herbes.php");
 $ID = $_GET['collectorID'];
-
-echo "
-    <head>
-        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
-        <link rel=\"stylesheet\" href=\"$CSSFile\" type=\"text/css\" />
-        <title> Sweden's Virtual Herbarium: Collector's record </title>
-        <meta name=\"robots\" content=\"noindex\" />
-        <meta name=\"author\" content=\"Nils Ericson\" />
-    </head>
-    <body id =\"collector\"> ";
-
 $con7 = getConS();
 $query = "SELECT Fornamn, Efternamn, Ful_Fornamn, Ful_Efternamn, birth, death, signatur FROM samlare JOIN signaturer ON samlare.ID = signaturer.samlar1_ID WHERE samlare.ID=:id";
 //echo $query. '<p>';
@@ -35,45 +44,27 @@ $Stm2->bindValue(':id', $ID, PDO::PARAM_INT);
 $Stm2->execute();
 $row2 = $Stm2->fetch(PDO::FETCH_ASSOC);
 
-
 echo "
-<div class = \"menu1\">
-        <ul>
-            <li class = \"start_page\"><a href=\"index.html\"> Start page </a></li>
-            <li class = \"standard_search\"><a href=\"standard_search.html\">Standard search</a> </li>
-            <li class = \"cross_browser\"><a href =\"cross_browser.php?SpatLevel=0&amp;SysLevel=0&amp;Sys=Life&amp;Spat=World&amp;Herb=All\">Cross browser</a> </li>
-        </ul>
-    </div>
-<div class = \"subMenu\">
-<h2> <span class = \"first\">S</span>weden's <span class = \"first\">V</span>irtual <span class = \"first\">H</span>erbarium: Collector's record </h2>
-<h3> $row[Fornamn] $row[Efternamn]";
-/*if ($user_id !=-1) {
-    echo " <a href=\"edit/edit_collector.php?collectorID=$ID\">Edit</a>";
-}*/
-echo "</h3>
-    <table class = \"outerBox\"> <tr> <td>";
-echo "
+<h3>$row[Fornamn] $row[Efternamn]</h3>
+    <table class = \"outerBox\"> <tr> <td>
         $row[Ful_Fornamn] $row[Ful_Efternamn] <br />
-        Born $row[birth]";
+        Born: $row[birth]";
 if($row['death'] != "") echo ", deceased $row[death]";
 echo "
-        <br /> <a href=\"list.php?CollectorID=$ID\"> $row2[nrRec]</a> specimens";
-echo "
+        <br /> <a href=\"list.php?CollectorID=$ID\">$row2[nrRec]</a> specimens
         <table>
-            <tr> <th> Recorded signatures: </th> </tr>
-            <tr> <td> $row[signatur] </td> </tr>";
+            <tr> <th>Recorded signatures:</th> </tr>
+            <tr> <td>$row[signatur]</td> </tr>";
 while($row = $Stm->fetch(PDO::FETCH_ASSOC)) {
     echo "
-            <tr> <td> $row[signatur] </td> </tr>";
+            <tr> <td>$row[signatur]</td> </tr>";
 }
-echo "
-        </table>";
 
-$con7=null;
-echo "
-    </td> </tr> </table>";
 if ($Logg == 'On')
     logg($MySQLHost, $MySQLLUser, $MySQLLPass);
 ?>
+    </table>
+    </td> </tr> </table>
+    </div>
     </body>
 </html>
